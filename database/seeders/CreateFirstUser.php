3ec6d5b1078c44ee7ef3006ef69216d4fb2bfaf9
stefanny233 <stefanny24si@mahasiswa.pcr.ac.id>
2025-11-10 09:04:39 +0700

    seeders

---
+++ database/seeders/CreateFirstUser.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class CreateFirstUser extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $data['name'] = 'pani';
+        $data['email'] = 'whatsupp@gmail.com';
+        $data['password'] = Hash::make('pani123');
+        User::create($data);
+    }
+}
